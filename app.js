adc43bed92451d5c4107df9bf5a59159fbf8ce4d
Dayan <dramirez5191@gmail.com>
2019-07-23 12:40:20 -0400

    Added partials directory and new routes

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const app = express();
 const rp = require('request-promise');
+const bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+const campgrounds = [
+  {name:"Salmon creek", image: "https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
+  {name:"Saltwater", image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
+  {name:"Lake Long", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
+  {name:"Mirror Lake", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}
+];
+
+//landing
 app.get('/', function(req,res){
   res.render('landing');
 });
 
+//shows all campgrounds
 app.get('/campgrounds', function(req,res){
-  const campgrounds = [
-    {name:"Salmon creek", image: "https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-    {name:"Saltwater", image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-    {name:"Lake Long", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-    {name:"Mirror Lake", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}
-  ];                             //Name : Data
+                                 //Name : Data
   res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+//logic of making a new campground and redirect to '/campgrounds'
+app.post('/campgrounds', function(req,res){
+  //get data from form and add to campground array
+  const name = req.body.name
+  const image = req.body.image
+  const newCampground = { name: name, image: image}
+  campgrounds.push(newCampground);
+  //redirect from campground arrya back to this route
+res.redirect('/campgrounds');
+});
+
+//form which submitts a post request to '/campgrounds' which redirects us to '/campgrounds'
+app.get('/campgrounds/new', function(req,res){
+  res.render('new');
 });
 
 app.listen(3001, function(){
