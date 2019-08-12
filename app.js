a6553f91b818e8466c7d3d1425cea6de341d6b92
Dayan <dramirez5191@gmail.com>
2019-08-12 13:47:02 -0400

    created version 2, integrating mongoDB

--- app.js
+++ app.js
@@ -1,51 +1,93 @@
-const express = require('express');
-const app = express();
-const rp = require('request-promise');
-const bodyParser = require('body-parser');
+const express     = require('express'),
+      app         = express(),
+      rp          = require('request-promise'),
+      bodyParser  = require('body-parser'),
+      mongoose    = require('mongoose')
+//connect to the mongodb DB
+mongoose.connect('mongodb://localhost/yelp_camp', {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const campgrounds = [
-  {name:"Salmon creek", image: "https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Saltwater", image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Lake Long", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Salmon creek", image: "https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Saltwater", image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Lake Long", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Salmon creek", image: "https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Saltwater", image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Lake Long", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"},
-  {name:"Mirror Lake", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"}
-];
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
 
+//compile schema into a model
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image:"https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg",
+//     description: "nice place to take your family, no bathroom, no water, no anything!"
+//   },
+//   function(err, campground){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("newly added campground");
+//     console.log(campground);
+//   }
+// });
 //landing
 app.get('/', function(req,res){
   res.render('landing');
 });
 
-//shows all campgrounds
+//INDEX - shows all campgrounds
 app.get('/campgrounds', function(req,res){
-                                 //Name : Data
-  res.render('campgrounds', {campgrounds: campgrounds});
+  // Get all campgrounds  form DB
+  Campground.find({},function(err,allCampgrounds){
+    if (err) {
+      console.log("THERE HAS BEEN AN ERROR");
+      console.log(err);
+    } else {
+      //renders campgrounds.ejs
+      res.render('index', {campgrounds: allCampgrounds});
+    }
+  });
 });
 
-//logic of making a new campground and redirect to '/campgrounds'
+//CREATE - Add ned campground to DB 
 app.post('/campgrounds', function(req,res){
-  //get data from form and add to campground array
-  const name = req.body.name
-  const image = req.body.image
-  const newCampground = { name: name, image: image}
-  campgrounds.push(newCampground);
-  //redirect from campground arrya back to this route
-res.redirect('/campgrounds');
+  //grabbing name and image from the form
+  const name = req.body.name;
+  const image = req.body.image;
+  const desc = req.body.description;
+  const newCampground = { name: name, image: image, description: desc};
+  //create new campground and save it to the DB
+  Campground.create(newCampground, function(err,newlyCreated){
+    if (err) {
+      console.log(err);
+    }else {
+      //redirect us back to the campground page
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
-//form which submitts a post request to '/campgrounds' which redirects us to '/campgrounds'
+// NEW Show form to create new campground
 app.get('/campgrounds/new', function(req,res){
   res.render('new');
 });
 
-app.listen(3001, function(){
+// SHOW - Get more information on the campground
+app.get('/campgrounds/:id', function(req,res){
+  Campground.findById(req.params.id, function(err,foundCampground){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('show', {campground: foundCampground})
+    }
+  });
+  //find the campground with provided id
+  //then render show template with that campground
+});
+
+app.listen(3002, function(){
   console.log("YelpCamp server has started");
 });
